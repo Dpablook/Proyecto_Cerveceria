1ed3d6042c4f22c8e50f4b13ecc4ebbcf18c4d9c
Dpablook <63682062+Dpablook@users.noreply.github.com>
2021-03-12 19:51:59 -0300

    Subido el proyecto inicial

---
+++ script.js
@@ -0,0 +1,12 @@
+/*Scroll*/
+
+window.addEventListener("scroll", function(nav){
+	var nav = document.querySelector(".nav");
+	nav.classList.toggle("scroll", window.scrollY > 0);
+})
+
+/*Me gusta*/
+
+function meGusta(){
+	
+}
